6655427b022294d78b4544a65e979da267a4041d
ha quang le <winlp4ever@gmail.com>
2019-05-20 00:40:12 +0200

    add animation for navi

--- navi.ts
+++ navi.ts
@@ -1,21 +1,28 @@
 var count = 0;
+
+var hidd = {'max-width': '0px', 'opacity': 0};
+var visi = {'max-width': '100px', 'opacity': 1};
+
 $(document).ready(function () {
-    $('#coding, #dl, .coding, .dl').css('display', 'none');
+    $('#coding, #dl').css(hidd);
+    $('.coding, .dl').css('display', 'none');
+
     $('#posts').click(function () {
-        if ($('#coding').css('display') == 'none') {
+        if ($('#coding').css('max-width') == '0px') {
             $('#posts').css('background-color', 'rgba(216, 139, 134, 0.2)');
-            $('#coding, #dl').css('display', 'inherit');
+            $('#coding, #dl').css(visi);
             $('.drleft i').css('transform', 'rotate(-90deg)');
         }
         else {
             $('#posts').css('background-color', 'inherit');
-            $('#coding, #dl').css('display', 'none');
+            $('#coding, #dl').css(hidd);
             $('.drleft i').css('transform', 'none');
         }
     });
     $('#home').click(function () {
         $('#posts').css('background-color', 'inherit');
-        $('#coding, #dl, .coding, .dl').css('display', 'none');
+        $('#coding, #dl').css(hidd);
+        $('.coding, .dl').css('display', 'none');
         $('.home').css('display', 'inherit');
         $('.drleft i').css('transform', 'none');
     })
