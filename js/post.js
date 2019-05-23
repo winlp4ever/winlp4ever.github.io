5097ddd4bc9b190cf7b4368290ac204c5952287d
ha quang le <winlp4ever@gmail.com>
2019-05-23 19:04:19 +0200

    fix post color

---
+++ js/post.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('.post b').prepend('#');
+});
---
+++ post.ts
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.post b').prepend('#');
+    $('.post h1').each(function () {
+        let me = $(this).parent();
+    })
+})
