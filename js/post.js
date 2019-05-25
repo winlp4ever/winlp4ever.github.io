3f8255f07603769c41784ec2bd1beca8f3d69c14
ha quang le <winlp4ever@gmail.com>
2019-05-25 02:18:13 +0200

    add tags

--- js/post.js
+++ js/post.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     $('.post b').prepend('#');
+    $('.post').each(function () {
+        var s = '';
+        var childs = $(this).children('p').toArray();
+        for (var i in childs) {
+            var desc = $(childs[i]).children('b').toArray();
+            for (var j in desc)
+                s += $(desc[j]).html() + " ";
+        }
+        $(this).prepend("<span>" + s + "</span>");
+    });
 });
--- post.ts
+++ post.ts
@@ -1,7 +1,12 @@
 $(document).ready(function () {
     $('.post b').prepend('#');
-    $('.post b').each(function () {
-        let me = $(this).parent();
-        
+    $('.post').each(function () {
+        let s = '';
+        var childs = $(this).children('p').toArray(); 
+        for (let i in childs) {
+            var desc = $(childs[i]).children('b').toArray();
+            for (let j in desc) s += $(desc[j]).html() + " ";
+        }
+        $(this).prepend(`<span>${s}</span>`);
     })
 })
