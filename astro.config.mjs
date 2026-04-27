65614a0e1c883b25271935aa5be43c67698967b7
Le Ha Quang <winlp4ever@gmail.com>
2026-04-27 14:53:24 +0200

    ci: deploy to GitHub Pages via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://winlp4ever.github.io',
   integrations: [react()],
   markdown: {
     shikiConfig: {
